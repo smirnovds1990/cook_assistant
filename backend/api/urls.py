2904ffd71bb53faa0e1f8968b8fad76e18edda74
smirnovds1990 <you@example.comsmirnovds1990@gmail.com>
2023-09-29 10:56:39 +0300

    Set basenames for the routers.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from .views import IngridientViewSet, RecipeViewSet, TagViewSet
 
 router = routers.DefaultRouter()
-router.register(r'tags', TagViewSet)
-router.register(r'recipes', RecipeViewSet)
-router.register(r'ingridients', IngridientViewSet)
+router.register(r'tags', TagViewSet, basename='tags')
+router.register(r'recipes', RecipeViewSet, basename='recipes')
+router.register(r'ingridients', IngridientViewSet, basename='ingridients')
 
 urlpatterns = router.urls
